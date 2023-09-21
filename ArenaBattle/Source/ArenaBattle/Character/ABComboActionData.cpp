40600686d0492ddfa7bd579cbd0dcff40d27df65
um-joon-sik <39338850+kimduuukbae@users.noreply.github.com>
2023-09-22 08:28:10 +0900

    Update ABComboActionData.cpp

--- ArenaBattle/Source/ArenaBattle/Character/ABComboActionData.cpp
+++ ArenaBattle/Source/ArenaBattle/Character/ABComboActionData.cpp
@@ -1,8 +1,6 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
 #include "Character/ABComboActionData.h"
 
 UABComboActionData::UABComboActionData()
 {
+	// 기준으로 정할게 없으면 정의하지 않아도 됨. (default)
 }
